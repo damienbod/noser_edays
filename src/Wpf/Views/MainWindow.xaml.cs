e4dd0c7d459525d0f23507d3d02dd9932bfa0348
olivier.corbat <olivier.corbat@noser.com>
2019-08-29 11:42:56 +0200

    ASP.NET Core SignalR .NET Client

--- src/Wpf/Views/MainWindow.xaml.cs
+++ src/Wpf/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Wpf.Views
 {
@@ -7,9 +10,65 @@ namespace Wpf.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        HubConnection connection;
         public MainWindow()
         {
             InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:53353/ChatHub")
+                .Build();
+
+            #region snippet_ClosedRestart
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+            #endregion
+        }
+
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            #region snippet_ConnectionOn
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    messagesList.Items.Add(newMessage);
+                });
+            });
+            #endregion
+
+            try
+            {
+                await connection.StartAsync();
+                messagesList.Items.Add("Connection started");
+                connectButton.IsEnabled = false;
+                sendButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            #region snippet_ErrorHandling
+            try
+            {
+                #region snippet_InvokeAsync
+                await connection.InvokeAsync("SendMessage",
+                    userTextBox.Text, messageTextBox.Text);
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+            #endregion
         }
     }
 }
